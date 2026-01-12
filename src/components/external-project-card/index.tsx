c6654bd966848219c0c9174761c6bbe5951510d3
Naethan Fajarito <148662719+NaethanF@users.noreply.github.com>
2026-01-11 21:14:29 -0800

    Update index.tsx

--- src/components/external-project-card/index.tsx
+++ src/components/external-project-card/index.tsx
@@ -102,7 +102,7 @@ const ExternalProjectCard = ({
             <div className="w-full">
               <div className="px-4">
                 <div className="text-center w-full">
-                  <h2 className="font-medium text-center opacity-60 mb-2">
+                  <h2 className="text-lg font-semibold text-center text-base-content mb-2">
                     {item.title}
                   </h2>
 
